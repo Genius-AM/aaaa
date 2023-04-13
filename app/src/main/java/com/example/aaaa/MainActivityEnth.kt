709f502a28d23d42cdf190ee474752d0726f3708
Amir <amir@avtoalfa.com>
2023-04-13 23:22:11 +0300

    test version

--- app/src/main/java/com/example/aaaa/MainActivityEnth.kt
+++ app/src/main/java/com/example/aaaa/MainActivityEnth.kt
@@ -1,12 +1,19 @@
 package com.example.aaaa
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.aaaa.R
 
 class MainActivityEnth : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_enth)
+    }
+
+    fun onClickEnth(view: View) {
+        val intent = Intent(this, OneWork::class.java)
+        startActivity(intent)
     }
 }
